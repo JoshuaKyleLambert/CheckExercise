d57db9cad5a48d38fa706a2204db8890b9d32267
Joshua <jl6356@stu.armstrong.edu>
2018-03-09 00:50:06 -0500

    CheckExerciseBean.java Managed bean added

--- src/java/CheckExercise/CheckExerciseBean.java
+++ src/java/CheckExercise/CheckExerciseBean.java
@@ -17,10 +17,194 @@ import java.io.Serializable;
 @SessionScoped
 public class CheckExerciseBean implements Serializable {
 
+    private String header;
+    private Boolean chapterSelectable;
+    private String[] chapter;
+    private String chapters;
+    private String programName;
+    private String[] exercises;
+    private String programStyle;
+    private String program;
+    private String inputPrompt;
+    private Boolean sampleDataProvided;
+    private String input;
+    private Boolean gradable;
+    private Boolean autoCheck;
+    private Boolean resultStyle;
+    private String gradeResult;
+    private String status;
+    private String result;
+
     /**
      * Creates a new instance of CheckExerciseBean
      */
     public CheckExerciseBean() {
     }
+
+    /**
+     * Actions and Listeners
+     *
+     */
+    public void changeChapter() {
+
+    }
+
+    public void chooseExercise() {
+
+    }
+
+    public void grade() {
+
+    }
+
+    public void run() {
+
+    }
+
+    public void checkURL() {
+
+    }
+
+    /**
+     * Getters and Setters
+     *
+     * @return
+     */
     
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getChapters() {
+        return chapters;
+    }
+
+    public void setChapters(String chapters) {
+        this.chapters = chapters;
+    }
+
+    public String getProgramName() {
+        return programName;
+    }
+
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+
+    public String[] getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(String[] exercises) {
+        this.exercises = exercises;
+    }
+
+    public String getProgramStyle() {
+        return programStyle;
+    }
+
+    public void setProgramStyle(String programStyle) {
+        this.programStyle = programStyle;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getInputPrompt() {
+        return inputPrompt;
+    }
+
+    public void setInputPrompt(String inputPrompt) {
+        this.inputPrompt = inputPrompt;
+    }
+
+    public Boolean isSampleDataProvided() {
+        return sampleDataProvided;
+    }
+
+    public void setSampleDataProvided(Boolean sampleDataProvided) {
+        this.sampleDataProvided = sampleDataProvided;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public Boolean isGradable() {
+        return gradable;
+    }
+
+    public void setGradable(Boolean gradable) {
+        this.gradable = gradable;
+    }
+
+    public Boolean isAutoCheck() {
+        return autoCheck;
+    }
+
+    public void setAutoCheck(Boolean autoCheck) {
+        this.autoCheck = autoCheck;
+    }
+
+    public Boolean getResultStyle() {
+        return resultStyle;
+    }
+
+    public void setResultStyle(Boolean resultStyle) {
+        this.resultStyle = resultStyle;
+    }
+
+    public String getGradeResult() {
+        return gradeResult;
+    }
+
+    public void setGradeResult(String gradeResult) {
+        this.gradeResult = gradeResult;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public Boolean isChapterSelectable() {
+        return chapterSelectable;
+    }
+
+    public void setChapterSelectable(Boolean chapterSelectable) {
+        this.chapterSelectable = chapterSelectable;
+    }
+
+    public String[] getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(String[] chapter) {
+        this.chapter = chapter;
+    }
+
 }
